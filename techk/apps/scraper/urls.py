41c158f6f0c3a4542bd816438a8f6fce5a96ce78
Rodrigo Gomez <rodrigosgg94@gmail.com>
2019-08-26 20:37:15 -0400

    Respaldo conseguido

--- techk/apps/scraper/urls.py
+++ techk/apps/scraper/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from .viewsets import BookViewSet
+from .viewsets import BookViewSet, ScraperView
 
 router = routers.SimpleRouter()
 router.register('books', BookViewSet)
+router.register(r'scraper/', ScraperView.as_view())
 
 urlpatterns = router.urls
--- techk/apps/scraper/viewsets.py
+++ techk/apps/scraper/viewsets.py
@@ -1,8 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
 from .models import Books
 from .serializer import BookSerializer
+
+from bs4 import BeautifulSoup
+import request
+
+
 
 class BookViewSet(viewsets.ModelViewSet):
 
     queryset = Books.objects.all()
     serializer_class = BookSerializer
+
+
+class ScraperView(generics.CreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+
+    def post(self, request):
+        return Response({"Ingresamos"},status=200)
